11ed82e852e33cdb2c3432f1b96243cd4701660b
Stev0439 <62709228+Stev0439@users.noreply.github.com>
2021-02-02 14:08:10 -0500

    Create test_hello.py

---
+++ test_hello.py
@@ -0,0 +1,16 @@
+'''
+Created on Feb 2, 2021
+
+@author: Steven
+'''
+import unittest
+
+class TestHello(unittest.TestCase):
+    # define multiple sets of tests as functions with names that begin
+
+    def testHello(self): 
+        self.assertEqual(1,1,'one equals one')
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
